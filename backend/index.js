88ca1b49e5bbcb4a5490b0696ff42db293a7a4a0
Fluke <jesadakorn.kirtnu@gmail.com>
2020-08-23 12:44:14 +0700

    fix bugs backend and frontend router

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Login = require('./login')
 
-app.use(require("./login"));
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }), Login)
+app.use(bodyParser.json, Login)
 
 app.listen(80, () => console.log('Server is ready!'))
--- backend/login.js
+++ backend/login.js
@@ -1,14 +1,14 @@
 require('tls').DEFAULT_MIN_VERSION = 'TLSv1'   // since TLSv1.3 default disable v1.0 
-const cors = require('cors')
 const express = require('express');
-const soap = require('soap');
 const bodyParser = require('body-parser')
+const soap = require('soap');
 const url = 'https://passport.psu.ac.th/authentication/authentication.asmx?wsdl';
 const app = express()
 const router = express.Router()
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }), router)
-app.use(bodyParser.json, router)
+// const cors = require('cors')
+// app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: false }), router)
+// app.use(bodyParser.json, router)
 
 router
     .route('/')
@@ -17,13 +17,14 @@ router
         console.log(req)
     })
     .post((req, res) => {
- 
+
         soap.createClient(url, (err, client) => {
             if (err) console.error(err);
             else {
                 let user = {}
                 user.username = req.body.username
                 user.password = req.body.password
+                console.log(user)
 
                 client.GetUserDetails(user, function (err, response) {
                     if (err) console.error(err);
@@ -36,5 +37,5 @@ router
         });
     })
 
-// module.exports = router
-app.listen(80, () => console.log('Server is ready!'))
+module.exports = router
+// app.listen(80, () => console.log('Server is ready!'))
